0cd35e37f6467f6a842b768645af0c7fcffec8dd
NAVEEN S <115558879+Navin3028@users.noreply.github.com>
2024-09-25 07:01:50 +0000

    Create test.py

---
+++ test.py
@@ -0,0 +1,29 @@
+import os
+
+def read_file(filename):
+    try:
+        with open(filename, 'r') as file:
+            return file.read()
+    except FileNotFoundError:
+        print("File not found.")
+        return None
+
+def get_user_input():
+    return input("Enter a command: ")
+
+def execute_command(command):
+    os.system(command)
+
+def main():
+    filename = get_user_input()
+    file_content = read_file(filename)
+    
+    if file_content:
+        print("File content:")
+        print(file_content)
+    
+    command = get_user_input()
+    execute_command(command)
+
+if __name__ == "__main__":
+    main()
